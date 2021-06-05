3b2c934989e644e7a7f796df055af737123c5c03
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-06-06 00:32:05 +0530

    bug fixes and configure tailwind

--- src/components/Herohome.js
+++ src/components/Herohome.js
@@ -1,68 +1,66 @@
 import React from "react";
 
 function HeroHome() {
-	return (
-		<section className="relative">
-			<div
-				className="absolute"
-				aria-hidden="true"
-			>
-				<svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
-					<defs>
-						<linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
-							<stop stopColor="#FFF" offset="0%" />
-							<stop stopColor="#FECCCB" offset="77.402%" />
-							<stop stopColor="#FECCCB" offset="100%" />
-						</linearGradient>
-					</defs>
-					<g fill="url(#illustration-01)" fillRule="evenodd">
-						<circle cx="1232" cy="48" r="128" />
-						<circle cx="255" cy="343" r="64" />
-						<circle cx="1232" cy="540" r="65" />
-					</g>
-				</svg>
-			</div>
+    let primaryThemeColor = "#FECCCB";
+    return (
+        <section className="relative">
+            <div
+                className="absolute"
+                aria-hidden="true"
+            >
+                <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+                            <stop stopColor="#FFF" offset="0%"/>
+                            <stop stopColor={primaryThemeColor} offset="77.402%"/>
+                            <stop stopColor={primaryThemeColor} offset="100%"/>
+                        </linearGradient>
+                    </defs>
+                    <g fill="url(#illustration-01)" fillRule="evenodd">
+                        <circle cx="1232" cy="48" r="128"/>
+                        <circle cx="255" cy="343" r="64"/>
+                        <circle cx="1232" cy="540" r="65"/>
+                    </g>
+                </svg>
+            </div>
 
-			<div className="max-w-6xl mx-auto px-4 sm:px-6">
-				<div className="pt-28 pb-12 md:pt-40 md:pb-20">
-					<div className="text-center pb-12 md:pb-16">
-						<h1
-							className="text-8xl font-extrabold leading-tighter tracking-tighter mb-4"
-							data-aos="zoom-y-out"
-						>
-							Online Invoice <br/>
-							<span className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-black-400">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
+                <div className="pt-28 pb-12 md:pt-40 md:pb-20">
+                    <div className="text-center pb-12 md:pb-16">
+                        <h1
+                            className="md:text-8xl text-7xl font-extrabold leading-tighter tracking-tighter mb-4"
+                            data-aos="zoom-y-out"
+                        >
+                            Online Invoice <br/>
+                            <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-black-400">
 								Generator
 							</span>
-						</h1>
-						<div className="max-w-3xl mx-auto">
-							<p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
-              It allows you rapidly to make invoices with our attractive invoice templates for various fields.
-              The invoices you make can be saved and downloaded as a PDF. <br />
-              <p className="font-bold my-2">Did we say its free of cost!</p>
-							</p>
-							<div
-								className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
-								data-aos="zoom-y-out"
-								data-aos-delay="300"
-							>
-								<div>
-									<button className="bg-black text-white font-bold rounded-md py-3 px-6 hover:bg-pink-700 w-full mb-4 sm:w-auto sm:mb-0">
-										Create Now
-									</button>
-								</div>
-								<div>
-									<button className="bg-black text-white font-bold rounded-md py-3 px-6 hover:bg-gray-800 w-full sm:w-auto sm:ml-4">
-										Learn more
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+                        </h1>
+                        <div className="max-w-3xl mx-auto">
+                            <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
+                                It allows you rapidly to make invoices with our attractive invoice templates for various
+                                fields.
+                                The invoices you make can be saved and downloaded as a PDF. <br/>
+                                <p className="font-bold my-2">Did we say its free of cost!</p>
+                            </p>
+                            <div
+                                className="flex md:flex-row flex-col md:justify-center md:space-x-7"
+                            >
+                                <button
+                                    className="bg-black text-white font-bold rounded-md py-3 px-6 transition-all duration-200 ease-in-out hover:bg-primary w-full mb-4 md:w-auto md:mb-0">
+                                    Create Now
+                                </button>
+                                <button
+                                    className="bg-black text-white font-bold rounded-md py-3 px-6 w-full md:w-auto transition-all duration-200 ease-in-out">
+                                    Learn more
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default HeroHome;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+      extend: {
+          colors: {
+              primary: "#FECCCB",
+          },
+      }
   },
   variants: {
     extend: {},
