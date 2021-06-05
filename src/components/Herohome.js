80ee85cd447d7cd849df52b07ff47e0e354682c7
adyasha <adyashamohanty8105@gmail.com>
2021-06-05 22:19:22 +0530

    Minor changes in UI

--- src/components/Herohome.js
+++ src/components/Herohome.js
@@ -4,7 +4,7 @@ function HeroHome() {
 	return (
 		<section className="relative">
 			<div
-				className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+				className="absolute"
 				aria-hidden="true"
 			>
 				<svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
@@ -17,14 +17,14 @@ function HeroHome() {
 					</defs>
 					<g fill="url(#illustration-01)" fillRule="evenodd">
 						<circle cx="1232" cy="48" r="128" />
-						<circle cx="155" cy="343" r="64" />
-						<circle cx="944" cy="540" r="65" />
+						<circle cx="255" cy="343" r="64" />
+						<circle cx="1232" cy="540" r="65" />
 					</g>
 				</svg>
 			</div>
 
 			<div className="max-w-6xl mx-auto px-4 sm:px-6">
-				<div className="pt-32 pb-12 md:pt-40 md:pb-20">
+				<div className="pt-28 pb-12 md:pt-40 md:pb-20">
 					<div className="text-center pb-12 md:pb-16">
 						<h1
 							className="text-8xl font-extrabold leading-tighter tracking-tighter mb-4"
