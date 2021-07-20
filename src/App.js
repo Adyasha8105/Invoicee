9134f5a0779d085eb22bebd380da6f01644622f8
Adyasha8105 <adyashamohanty8105@gmail.com>
2021-07-20 22:00:08 +0530

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Invoice from "./pages/Invoice";
-
 import Home from "./pages/Home";
+import ReactGA from 'react-ga';
 
 function App() {
+
+	if (process.browser) {
+		ReactGA.initialize('G-Y88062DSY8')
+		ReactGA.pageview(window.location.pathname + window.location.search)
+	}
+
 	return (
 		<BrowserRouter>
 			<Switch>
