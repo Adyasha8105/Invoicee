1bc2d2f6acc67d662e7c3c14b7e86cb10a478e77
Adyasha8105 <adyashamohanty8105@gmail.com>
2021-07-20 23:34:09 +0530

    Add google analytics both universal and g tag

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Invoice from "./pages/Invoice";
 import Home from "./pages/Home";
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
 
 function App() {
+  if (process.browser) {
+    ReactGA.initialize("UA-167474329-2");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
 
-	if (process.browser) {
-		ReactGA.initialize('G-Y88062DSY8')
-		ReactGA.pageview(window.location.pathname + window.location.search)
-	}
-
-	return (
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/invoice">
-					<Invoice />
-				</Route>
-			</Switch>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/invoice">
+          <Invoice />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
